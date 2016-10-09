8c3b3100e423a8516120866603cb8595258a7b62
jacob <2staffi@gmail.com>
2016-10-09 16:34:01 -0400

    took out alert box and post under text box

--- public/js/form_validation.js
+++ public/js/form_validation.js
@@ -1,7 +1,16 @@
 function validateForm() {
     var value = document.forms["name_form"]["input_name"].value;
     if (value.length < 3) {
-        alert ("Try Again, but this time enter your name!")
+        document.getElementById("error").innerHTML = "Try Again but this time enter your name!";
         return false;
     }
-} 
+}
+
+
+// function validateForm(form,box) {
+// var value = document.forms[form][box].value;
+//  if (value.length < 3) {
+//      document.getElementById("error").innerHTML = "Try Again but this time enter your name!";
+//      return false;
+//  }
+// }
