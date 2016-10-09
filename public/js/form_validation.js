f188727119875b0338e19e9268e98acf745f556f
jacob <2staffi@gmail.com>
2016-10-09 16:34:43 -0400

    took out notes

--- public/js/form_validation.js
+++ public/js/form_validation.js
@@ -5,12 +5,3 @@ function validateForm() {
         return false;
     }
 }
-
-
-// function validateForm(form,box) {
-// var value = document.forms[form][box].value;
-//  if (value.length < 3) {
-//      document.getElementById("error").innerHTML = "Try Again but this time enter your name!";
-//      return false;
-//  }
-// }
