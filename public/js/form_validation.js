78d505afc3e3676d9d3ae6c343c5bcdfdbb7c2b1
jacob <2staffi@gmail.com>
2016-10-13 11:36:32 -0400

    wording

--- public/js/form_validation.js
+++ public/js/form_validation.js
@@ -1,7 +1,7 @@
 function validateForm() {
     var value = document.forms["name_form"]["input_name"].value;
     if (value.length < 3) {
-        document.getElementById("error").innerHTML = "Try Again but this time enter your name!";
+        document.getElementById("error").innerHTML = "Try again but this time enter your name!";
         return false;
     }
 }
