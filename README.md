19693181cbf63b52ab4cf8f4a39a538c5fb37b1a
2staffi@gmail.com <2staffi@gmail.com>
2017-06-07 17:07:14 -0400

    junk

--- README.md
+++
@@ -1,4 +0,0 @@
-# Personal Details Web App
-This repository houses my first attempt at a web app with html, css, and javascript. It uses my coin changer script and my personal details app.
-
-Run the app.rb file in terminal to see what it is about.
--- public/js/form_validation.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    var value = document.forms["name_form"]["input_name"].value;
-    if (value.length < 3) {
-        document.getElementById("error").innerHTML = "Try again but this time enter your name!";
-        return false;
-    }
-}
